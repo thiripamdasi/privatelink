795146fb8b7ae62451de6e940e80a0a84f33e8bb
thiripamdasi <165246633+thiripamdasi@users.noreply.github.com>
2024-03-28 11:48:24 +0530

    Update teststorage.ps1

--- teststorage.ps1
+++ teststorage.ps1
@@ -1,2 +1,2 @@
 Connect-AzAccount -UseDeviceAuthentication 
-New-AzStorageAccount -name practeststorage1 -ResourceGroupName privatelink-prac2 -SkuName Standard_LRS -location centralus -kind Storagev2
+New-AzStorageAccount -name practeststorage2 -ResourceGroupName privatelink-prac2 -SkuName Standard_LRS -location centralus -kind Storagev2
