b59f45444f86b11d4bf54a1c13bae52fed17981b
thiripamdasi <165246633+thiripamdasi@users.noreply.github.com>
2024-03-28 11:24:03 +0530

    Update deploy1.ps1

--- deploy1.ps1
+++ deploy1.ps1
@@ -1,9 +1,9 @@
 param (
-    [string] $resourceGroupName = "privatelink-prac",
+    [string] $resourceGroupName = "privatelink-prac2",
     [string] $deploymentName = "VirtualNetworkLinkDeployment",
     [string] $templateFilePath = "vnetlinks.json",
     [string] $privateDnsZoneName = "testingprivatedns.com",
-    [string] $virtualNetworkLinkName = "testingvnetlink",
+    [string] $virtualNetworkLinkName = "testingvnetlink2",
     [string] $virtualNetworkId = "/subscriptions/903a670d-9ae6-42f6-8495-12fd1d7e01cf/resourceGroups/privatelink-prac2/providers/Microsoft.Network/virtualNetworks/vnet01"
 )
 
